d7b47fc492d92261db76a431a2f54c67a4311779
xiaohuahua <2404051686@qq.com>
2021-01-11 15:16:51 +0800

    1.实体信息

---
+++ src/shiti/Passanger.java
@@ -0,0 +1,42 @@
+package shiti;
+
+public class Passanger
+{
+    String Id;
+    String name;
+    String phone;
+    String CardId;
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCardId() {
+        return CardId;
+    }
+
+    public void setCardId(String cardId) {
+        CardId = cardId;
+    }
+
+
+}
